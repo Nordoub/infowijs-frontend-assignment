f4415861b01a3a0cfd088752ab4172d9c52280ba
Norddin Oubahman <n.oubahman@hotmail.com>
2025-03-31 20:12:09 +0200

    Install required libraries

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,20 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import "./constants/domPurify.ts";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { RouterProvider } from "react-router-dom";
+import router from "./routing/routes.tsx";
+import FeedProvider from "./context/FeedProvider.tsx";
 
-createRoot(document.getElementById('root')!).render(
+const queryClient = new QueryClient();
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <QueryClientProvider client={queryClient}>
+      <FeedProvider>
+        <RouterProvider router={router} />
+      </FeedProvider>
+    </QueryClientProvider>
+  </StrictMode>
+);
