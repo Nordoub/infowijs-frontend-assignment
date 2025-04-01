7b368d10e207cc2bd4dbdd6e19eecf0eff19c8e3
Norddin Oubahman <n.oubahman@hotmail.com>
2025-04-01 13:05:20 +0200

    Reset scroll to top on page change

--- src/routing/Layout.tsx
+++ src/routing/Layout.tsx
@@ -1,16 +1,30 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
+import { PropsWithChildren, useLayoutEffect } from "react";
+
+const Wrapper = ({ children }: PropsWithChildren) => {
+  const location = useLocation();
+
+  useLayoutEffect(() => {
+    // Scroll to the top of the page when the route changes
+    window.scrollTo({ top: 0, left: 0, behavior: "instant" });
+  }, [location.pathname]);
+
+  return children;
+};
 
 const Layout = () => {
   return (
-    <div className="min-h-screen flex flex-col justify-between">
-      <Header />
-      <main className="2xl:w-60/100 w-80/100 justify-center place-self-center ">
-        <Outlet />
-      </main>
-      <Footer />
-    </div>
+    <Wrapper>
+      <div className="min-h-screen flex flex-col justify-between">
+        <Header />
+        <main className="2xl:w-60/100 w-80/100 justify-center place-self-center ">
+          <Outlet />
+        </main>
+        <Footer />
+      </div>
+    </Wrapper>
   );
 };
 
