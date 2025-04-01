022b5ee7e6a88852bda21c0e06aa929417bb9fb3
Norddin Oubahman <n.oubahman@hotmail.com>
2025-04-01 13:10:46 +0200

    Add default staletime to queryclient

--- src/main.tsx
+++ src/main.tsx
@@ -7,7 +7,11 @@ import { RouterProvider } from "react-router-dom";
 import router from "./routing/routes.tsx";
 import FeedProvider from "./context/FeedProvider.tsx";
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: { staleTime: 1000 * 60 * 15 },
+  },
+});
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
